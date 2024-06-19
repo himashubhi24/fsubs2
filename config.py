3a30ce9410fb27f3848059cbbc2ea296bc160534
Miko756 <164617880+Miko756@users.noreply.github.com>
2024-06-19 13:54:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "29759992"))
 API_HASH = os.environ.get("API_HASH", "61f150cdca64b2916fa499d107393140")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002071132786"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002077715264"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "5787502520"))
